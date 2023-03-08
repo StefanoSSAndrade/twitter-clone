dec3cf34050b4baf00ee5201c0d477929d5111b4
stefanossandrade <stefano.ss.andrade@hotmail.com>
2023-03-07 21:53:53 -0300

    first commit

---
+++ ui-twitter/src/components/Tweet.tsx
@@ -0,0 +1,5 @@
+export const Tweet = () => {
+    return (
+        <div><h1>Tweet</h1></div>
+    )
+}
---
+++ ui-twitter/src/main.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { Tweet } from './components/Tweet'
+
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+  <React.StrictMode>
+    <Tweet />
+  </React.StrictMode>,
+)
