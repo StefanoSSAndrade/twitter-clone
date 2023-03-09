ff9d6eb753c1a4cf43374d047ed7fa9c7c87d052
stefanossandrade <stefano.ss.andrade@hotmail.com>
2023-03-08 22:35:13 -0300

    css update

--- ui-twitter/src/components/Tweet.tsx
+++ ui-twitter/src/components/Tweet.tsx
@@ -1,5 +1,26 @@
+import styles from './Tweet.module.css'
+
 export const Tweet = () => {
     return (
-        <div><h1>Tweet</h1></div>
+        <a href="#" className="tweet">
+            <img src="https://github.com/stefanossandrade.png" alt="Stefano Andrade" />
+
+            <div className="tweet-content">
+                <div className="tweet-content-header">
+                    <strong>Estevão Safadão</strong>
+                    <span>@stefanossandrade</span>
+                </div>
+
+                <p>
+                    Acabei de migrar um projeto React GIGANTE de create-react-app para Vite e os resultados foram:
+                    <br />
+                    ✅ npm start: De 32s para 400ms (sim, demorava 30s)
+                    <br />
+                    ✅ npm build: De 120s para 22s
+                    <br />
+                    Além disso, troquei do Yarn para o PNPM e o install das deps mudou de 24s para 8s 🔥
+                </p>
+            </div>
+        </a>
     )
 }
--- ui-twitter/src/main.tsx
+++ ui-twitter/src/main.tsx
@@ -1,9 +1,78 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { House, Hash, Bell, Envelope, BookmarkSimple, FileText, User, DotsThreeCircle, Sparkle } from 'phosphor-react'
+
+import twtterLogo from './assets/logo-twitter.svg'
 import { Tweet } from './components/Tweet'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <Tweet />
-  </React.StrictMode>,
+    <div className="layout">
+      <aside className="sidebar">
+        <img className="logo" src={twtterLogo} alt="logo" />
+
+        <nav className="main-navigation">
+          <a className="active" href="">
+            <House weight='fill' />
+            Home
+          </a>
+          <a href="">
+            <Hash />
+            Explore
+          </a>
+          <a href="">
+            <Bell />
+            Notifications
+          </a>
+          <a href="">
+            <Envelope />
+            Messages
+          </a>
+          <a href="">
+            <BookmarkSimple />
+            Bookmarks
+          </a>
+          <a href="">
+            <FileText />
+            Lists
+          </a>
+          <a href="">
+            <User />
+            Profile
+          </a>
+          <a href="">
+            <DotsThreeCircle />
+            More
+          </a>
+        </nav>
+
+        <button className="new-tweet" type="button">
+          Tweet
+        </button>
+      </aside>
+      <div className="content">
+        <main className="timeline">
+          <div className="timeline-header"></div>
+          Home
+          <Sparkle />
+
+          <form className="new-tweet-form">
+            <label htmlFor="tweet">
+              <img src="https://github.com/stefanossandrade.png" alt="Stefano Andrade" />
+              <textarea id="tweet" placeholder="What's happening?"></textarea>
+            </label>
+
+            <button type="submit">Tweet</button>
+          </form>
+
+          <div className="separator" />
+
+          <Tweet />
+          <Tweet />
+          <Tweet />
+          <Tweet />
+        </main>
+      </div>
+    </div>
+  </React.StrictMode>
 )
